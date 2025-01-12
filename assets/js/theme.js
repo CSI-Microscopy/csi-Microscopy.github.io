d5fd4d6267f601159e96cf9e5ea6f036fb90d300
Daniel-Waiger <55537771+Daniel-Waiger@users.noreply.github.com>
2025-01-12 14:16:05 +0200

    update

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,12 +1,7 @@
-function toggleTheme() {
-    const html = document.documentElement;
-    const currentTheme = html.getAttribute('data-theme');
-    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-    
-    html.setAttribute('data-theme', newTheme);
-    localStorage.setItem('theme', newTheme);
-}
-
-const savedTheme = localStorage.getItem('theme') || 'light';
-document.documentElement.setAttribute('data-theme', savedTheme);
-document.getElementById('theme-toggle').addEventListener('click', toggleTheme);
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', () => {
+  const html = document.documentElement;
+  const currentTheme = html.getAttribute('data-theme') || 'light';
+  const newTheme = (currentTheme === 'light') ? 'dark' : 'light';
+  html.setAttribute('data-theme', newTheme);
+});
